561e0d7cbd807bde2cd2579b3f6212e921e7fcc2
sachin saluja <sachin.saluja132@gmail.com>
2012-05-26 00:29:27 +0530

    implement cache

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  // app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
+  app.use(express.static(__dirname + '/public'));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
