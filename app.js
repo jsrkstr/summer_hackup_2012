244e87db32b77c8048c2d1b58cb853b78878b0ec
sachin saluja <sachin.saluja132@gmail.com>
2012-05-26 00:14:31 +0530

    implement cache control

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.configure('development', function(){
 });
 
 app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
   app.use(express.errorHandler());
 });
 
